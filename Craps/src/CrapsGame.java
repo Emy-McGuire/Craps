f3f1545f608ee7195ef4ac0542e43887ff32ddc6
emma.mcguire <emma.mcguire@MU-C1-05.mullen.pvt.k12.co.us>
2019-08-23 11:45:15 -0600

    Done!

--- Craps/src/CrapsGame.java
+++ Craps/src/CrapsGame.java
@@ -25,41 +25,54 @@ public class CrapsGame
 				while (answer)
 					{
 				
-				if (total == 11)
+				if (total == 11 && total == 7)
 					{
 						System.out.println("You win, " + name + "!");
 						answer = false;
 					}
-				if (total == 7)
+				
+				
+				if (total == 2 && total == 12)
 					{
-						System.out.println("You win, " + name + "!");
+						System.out.println("Oh no! You loose, " + name + "!");
 						answer = false;
+
+					}
+				else
+					
+					{
+						System.out.println("Press enter to roll again!");
+						String fake = userInput.nextLine();
+						
+							int roll3 = (int) (Math.round(Math.random()* 6 + 1));
+							int roll4 = (int) (Math.round(Math.random()* 6 + 1));
+						
+							int total2 = roll3 + roll4;
+						
+									
+						System.out.println("Your point is " + total + ". You rolled " + roll3 + " and " + roll4 + " for a total of " + total2 + "!");
+						
+						if (total == total2)
+							{
+								System.out.println("Congrats, " + name + "! You win!!");
+								answer = false;
+		
+							}
+						
+						if (total2 == 7)
+							
+							{
+								System.out.println("Sorry, " + name + "! YOU LOSE!!");
+								answer = false;
+							}
+						
+					}
 
 					}
 				
-				if (total == 2)
-					{
-						System.out.println("Oh no! You loose, " + name + "!");
-						answer = false;
+		
 
-					}
-				else if (total == 12)
-					{
-						System.out.println("Oh no! You loose, " + name + "!");
-						answer = false;
-					}
-						
-				else 
-						{
-							System.out.println("Roll again!");
-							int roll3 = (int) (Math.round(Math.random()* 6 + 1));
-							int roll4 = (int) (Math.round(Math.random()* 6 + 1));
-							
-							System.out.println("You rolled " + roll3 + " and " + roll4 + " for a total of " + total + "!");
-
-						}
-
-					}
+					
 					}
 			
 			
